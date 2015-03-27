56b9e8e0cb67396ad00c012f4ebe14ef58dc00e6
Ryan Haase <ryandhaase@gmail.com>
2015-03-27 10:43:58 -0400

    Finalizing creating and destroying topics, posts, and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :post 
   belongs_to :user
 
-  default_scope {order('created_at DESC')}
-
   validates :body, length: { minimum: 5 }, presence: true
   validates :user_id, presence: true
+
+  default_scope { order('updated_at DESC') }
+
 end
